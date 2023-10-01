fc310429ed2cabaeaaf809d8fa7f2572eb856f3a
Nada Khas <Nad1071996@outlook.com>
2023-10-01 16:17:57 +0300

    new coding challenge

---
+++ countingSheep.js
@@ -0,0 +1,12 @@
+/*Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).*/
+
+function countSheeps(sheep) {
+  // TODO
+  let sum = 0;
+  sheep.forEach(e => {
+    if (e === true){
+      sum++;
+    }
+  })
+  return sum
+}
